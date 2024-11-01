641a021190e37943844b2c0f9492d86f2fabfd0d
ITboo <annaacdc@tut.by>
2024-11-01 21:31:36 +0300

    feat: add books repository

--- src/modules/books/books.module.ts
+++ src/modules/books/books.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
-import { BooksService } from './books.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Book } from './book.entity';
+import { BooksRepository } from './books.repository';
 
 @Module({
-  providers: [BooksService],
+  imports:[TypeOrmModule.forFeature([Book])],
+  providers: [BooksService, BooksRepository],
   controllers: [BooksController]
 })
 export class BooksModule {}

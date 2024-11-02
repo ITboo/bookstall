63923440a1c558554b8e7c93a9f950db670d846a
ITboo <annaacdc@tut.by>
2024-11-02 22:28:54 +0300

    feat: add get, post operations

--- src/modules/books/books.controller.ts
+++ src/modules/books/books.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { BooksService } from './books.service';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  constructor(private readonly booksService: BooksService) {}
+  @Get()
+  async getAllBooks() {
+    return this.booksService.getAllBooks();    
+  }
+
+  @Get(':id')
+  async getBookById(@Param('id') id: number) {
+    return this.booksService.getBookById(id); 
+  }
+
+  @Post()
+  @HttpCode(201)
+  async createBook(@Body() bookDto: any) {
+    return this.booksService.createBook(bookDto);
+  }
+
+  @Put(':id')
+  async updateBook(@Param('id') id: number, @Body() bookDto:any) {}
+
+  @Delete(':id')
+  async deleteBook(@Param('id') id: number) {}
+}
--- src/modules/books/books.service.ts
+++ src/modules/books/books.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { BooksRepository } from './books.repository';
+import { Book } from './book.entity';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+  constructor(private readonly booksRepository: BooksRepository) {}
+  async getAllBooks(): Promise<Book[]> {
+    return this.booksRepository.findAll();
+  }
+  async getBookById(id: number): Promise<Book> {
+    return this.booksRepository.findOneOrNotFoundFail(id);
+  }
+  async createBook(dto: any): Promise<void> {
+    const book: Book = new Book();
+    book.title = dto.title;
+    book.ageRestriction = dto.ageRestriction;
+    book.author = dto.author;
+
+    await this.booksRepository.save(book);
+  }
+}
